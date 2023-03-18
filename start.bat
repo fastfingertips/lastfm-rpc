d822b70562f6f0132c1f8226f4ecb0c9330174a5
FastFingertips <bugra.coskun@outlook.com>
2023-03-18 22:56:45 +0300

    Update start.bat to use pythonw for running main.pyw in the background

--- start.bat
+++ start.bat
@@ -1,1 +1,7 @@
-pyw main.py
+@echo off
+
+REM Start the Last.fm RPC script using Pythonw
+pythonw main.pyw
+
+REM Pause the script so the console window stays open after the script is done running
+pause
