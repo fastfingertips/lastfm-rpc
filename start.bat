96e5ff64c721596fbf79423a66df44e1c1243352
FastFingertips <bugra.coskun@outlook.com>
2023-03-19 23:03:16 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM Start the Last.fm RPC script using Pythonw
-pythonw main.pyw
+pythonw main.py
 
 REM Pause the script so the console window stays open after the script is done running
 pause
