0b2bc14a9f9dba303909314de5cdb13fc97f5289
FastFingertips <bugra.coskun@outlook.com>
2024-07-24 20:26:51 +0300

    Update project.py

--- constants/project.py
+++ constants/project.py
@@ -1,3 +1,8 @@
+from helpers.reader import load_config, load_translations
+
+USERNAME, API_KEY, API_SECRET, APP_LANG = load_config()
+
 CLIENT_ID = '702984897496875072'
+TRANSLATIONS = load_translations(APP_LANG)
 DEFAULT_AVATAR_ID = "818148bf682d429dc215c1705eb27b98"
 DEFAULT_AVATAR_URL = f"https://lastfm.freetls.fastly.net/i/u/avatar170s/{DEFAULT_AVATAR_ID}.png"
