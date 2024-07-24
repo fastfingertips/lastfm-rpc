1768b2005c4b191310abdb470af8ce928a7758fe
FastFingertips <bugra.coskun@outlook.com>
2024-07-24 17:42:54 +0300

    update: discord_rpc

--- discord_rpc.py
+++ discord_rpc.py
@@ -1,50 +1,53 @@
-import os
-import json
-import datetime
-from pypresence import Presence
 from helpers.request_utils import get_response, get_dom
 from helpers.string_utils import get_removal
 from helpers.url_utils import url_encoder
+from pypresence import Presence
+import datetime
+import json
+import os
 
-client_id = '702984897496875072'
-RPC = Presence(client_id)
+CLIENT_ID = '702984897496875072'
+RPC = Presence(CLIENT_ID)
 enabled = False
 disabled = True
 start_time = None
 last_track = None
 
-def get_user_data(username):
-    user_profile_url = f'https://www.last.fm/user/{username}'
+def get_user_data(username) -> dict:
 
-    while True:
-        response = get_response(user_profile_url)
-        if response.status_code in range(200,299):
-            # Display Name
-            def parse_user_display_name(page_content): # get the display name of the user
+    USER_PROFILE_URL = f'https://www.last.fm/user/{username}'
+    DEFAULT_AVATAR_ID = "818148bf682d429dc215c1705eb27b98"
+    DEFAULT_AVATAR_URL = f"https://lastfm.freetls.fastly.net/i/u/avatar170s/{DEFAULT_AVATAR_ID}.png"
+
+    def parse_user_display_name(page_content):
+                # get the display name of the user
                 display_name = page_content.find("span", {"class":"header-title-display-name"})
                 display_name = display_name.text.strip()
                 return display_name
 
-            # Avatar Url
-            def parse_user_avatar_url(page_content): # get the avatar url of the user
-                defaultAvatarId = "818148bf682d429dc215c1705eb27b98" # default avatar id
-                defaultImageUrl = f"https://lastfm.freetls.fastly.net/i/u/avatar170s/{defaultAvatarId}.png" # default avatar url
-                profileAvatarUrl = page_content.find("meta", property="og:image")["content"] # find the profile avatar url
-                profileAvatarUrl = profileAvatarUrl.replace("/avatar170s","") # remove the size of the avatar
-                avatarSuffix = os.path.splitext(profileAvatarUrl)[1] # get the suffix of the avatar
-                profileAvatarUrl = profileAvatarUrl.replace(avatarSuffix,".gif") # replace the suffix with .gif
-                if defaultAvatarId in profileAvatarUrl:
-                    profileAvatarUrl = None # "No Avatar (Last.fm default avatar)"
-                return profileAvatarUrl
+    def parse_user_avatar_url(page_content):
+        # get the avatar url of the user
+        user_avatar_url = page_content.find("meta", property="og:image")["content"] # find the profile avatar url
+        user_avatar_url = user_avatar_url.replace("/avatar170s","") # remove the size of the avatar
+        avatar_suffix = os.path.splitext(user_avatar_url)[1] # get the suffix of the avatar
+        user_avatar_url = user_avatar_url.replace(avatar_suffix, ".gif") # replace the suffix with .gif
+        if DEFAULT_AVATAR_ID in user_avatar_url:
+            # "No Avatar (Last.fm default avatar)"
+            user_avatar_url = None
+        return user_avatar_url
 
-            # Header Status
-            def parse_user_header_status(page_content): # get the header status of the user
-                headerStatus = [0, 0, 0]
-                headers = page_content.find_all("div", {"class": "header-metadata-display"})
-                for i in range(len(headers)):
-                    headerStatus[i] = headers[i].text.strip()
-                    headerStatus[i] = get_removal(headerStatus[i],',', int) # {} içerisindeki {}'i kaldır ve {} olarak geri al.
-                return headerStatus
+    def parse_user_header_status(page_content):
+        # get the header status of the user
+        header_status = [0, 0, 0]
+        headers = page_content.find_all("div", {"class": "header-metadata-display"})
+        for i in range(len(headers)):
+            header_status[i] = headers[i].text.strip()
+            header_status[i] = get_removal(header_status[i],',', int) # {} içerisindeki {}'i kaldır ve {} olarak geri al.
+        return header_status
+
+    while True:
+        response = get_response(USER_PROFILE_URL)
+        if response.status_code in range(200,299):
 
             dom = get_dom(response)
             data = {
@@ -57,7 +60,6 @@ def get_user_data(username):
 
 def get_library_data(username, artistName, trackName):
 
-    # get artist info
     def parse_artist_count(dom):
         data = dom.find_all("p", {"class":"metadata-display"})
         if bool(data):
@@ -67,7 +69,6 @@ def get_library_data(username, artistName, trackName):
             data = 0
         return data
 
-    # get track info
     def parse_track_count(dom):
         data = dom.find_all("p", {"class":"metadata-display"})
         if bool(data):
@@ -105,111 +106,116 @@ def update_status(track, title, artist, album, time_remaining, username, artwork
     # print name and value
     for f in [track, title, artist, album, time_remaining, username, artwork]:
         print(f)
+
     global start_time, last_track
-    if len(title) < 2: title = title + ' '
-    if last_track == track: pass # if the track is the same as the last track, don't update the status
-    else: # if the track is different, update the status
-        albumBool = album is not None
-        timeRemainingBool = time_remaining > 0
-        if timeRemainingBool:
-            time_remaining = str(time_remaining)[0:3]
-        
-        print(f'Album: {albumBool:5}: {album}')
-        print(f'Time Remaining: {timeRemainingBool}: {time_remaining}')
+
+    if len(title) < 2:
+        title = title + ' '
+
+    if last_track == track:
+        # if the track is the same as the last track, don't update the status
+        pass
+    else:
+        # if the track is different, update the status
+        album_bool = album is not None
+        time_remaining_bool = time_remaining > 0
+        if time_remaining_bool:
+            time_remaining = float(str(time_remaining)[0:3])
+
+        print(f'Album: {album_bool:5}: {album}')
+        print(f'Time Remaining: {time_remaining_bool}: {time_remaining}')
         print("Now Playing: " + track)
 
         start_time = datetime.datetime.now().timestamp()
         last_track = track
-        trackArtistAlbum = f'{artist} - {album}'
+        track_artist_album = f'{artist} - {album}'
 
         rpcButtons = [
-        {"label": "View Track",
-         "url": str(f"https://www.last.fm/tr/user/{username}/library/music/{url_encoder(artist)}/_/{url_encoder(title)}")},
-        {"label": "Search on Spotify",
-         "url": str(f"https://open.spotify.com/search/{url_encoder(album)}")}]
-       #{"label": "View Track",
-       # "url": str(f"https://www.last.fm/music/{url_encoder(artist)}/{url_encoder(title)}")}
-       #{"label": "View Last.fm Profile",
-       # "url": str(f"https://www.last.fm/user/{username}")}
+        {"label": "View Track", "url": str(f"https://www.last.fm/tr/user/{username}/library/music/{url_encoder(artist)}/_/{url_encoder(title)}")},
+        {"label": "Search on YouTube Music", "url": str(f"https://music.youtube.com/search?q={url_encoder(album)}")}
+       #{"label": "Search on Spotify", "url": str(f"https://open.spotify.com/search/{url_encoder(album)}")}
+       #{"label": "View Track", "url": str(f"https://www.last.fm/music/{url_encoder(artist)}/{url_encoder(title)}")}
+       #{"label": "View Last.fm Profile", "url": str(f"https://www.last.fm/user/{username}")}
+        ]
 
         user_data = get_user_data(username)
+        print(json.dumps(user_data, indent=2))
 
         user_display_name = user_data["display_name"]
         scrobbles, artists, loved_tracks = user_data["header_status"] # unpacking
 
-        print(json.dumps(user_data, indent=2))
         library_data = get_library_data(username, artist, title)
         print(json.dumps(library_data, indent=2))
 
-        rpcSmallImage = user_data["avatar_url"]
+        rpc_small_image = user_data["avatar_url"]
+        artist_count = library_data["artist_count"]
 
-        smallImageLines = {
+        large_image_lines = {}
+        small_image_lines = {
             'name':         f"{user_display_name} (@{username})",
             "scrobbles":    f'Scrobbles: {scrobbles}',
             "artists":      f'Artists: {artists}',
-            "loved_tracks": f'Loved Tracks: {loved_tracks}'
-            }
-
-        largeImageLines = {}
-        artistCount = library_data["artist_count"]
+            "loved_tracks": f'Loved Tracks: {loved_tracks}'}
 
         # artwork
         if artwork == None: # if there is no artwork, use the default one
             now = datetime.datetime.now()
-            isDay = now.hour >= 18 or now.hour < 9  #day: false, night: true
-            artwork = 'https://i.imgur.com/GOVbNaF.png' if isDay else 'https://i.imgur.com/kvGS4Pa.png'
-            largeImageLines['theme'] = f"{'Night' if isDay else 'Day'} Mode Cover"
+            is_day = now.hour >= 18 or now.hour < 9  #day: false, night: true
+            artwork = 'https://i.imgur.com/GOVbNaF.png' if is_day else 'https://i.imgur.com/kvGS4Pa.png'
+            large_image_lines['theme'] = f"{'Night' if is_day else 'Day'} Mode Cover"
         else: pass
 
-        if artistCount: # if the artist is in the library
-            trackCount = library_data["track_count"]
-            largeImageLines["artist_scrobbles"] = f'Scrobbles: {artistCount}/{trackCount}' if trackCount else f'Scrobbles: {artistCount}'
+        if artist_count: # if the artist is in the library
+            track_count = library_data["track_count"]
+            large_image_lines["artist_scrobbles"] = f'Scrobbles: {artist_count}/{track_count}' if track_count else f'Scrobbles: {artist_count}'
         else:
-            largeImageLines['first_time'] = f'First time listening!'
+            large_image_lines['first_time'] = f'First time listening!'
 
         # line process
-        rpcSmallImageText = ''
-        rpcLargeImageText = ''
+        rpc_small_image_text = ''
+        rpc_large_image_text = ''
         lineLimit = 26
         xchar = ' '
 
-        print(len(largeImageLines), largeImageLines)
-        for line_key in smallImageLines:
-            line = f'{smallImageLines[line_key]} '
+        print(len(large_image_lines), large_image_lines)
+        for line_key in small_image_lines:
+            line = f'{small_image_lines[line_key]} '
             lineSuffix = "" if len(line) > 20 else (lineLimit - len(line) - sum(_.isupper() for _ in line))*xchar
-            rpcSmallImageText += f'{line}{lineSuffix} '
+            rpc_small_image_text += f'{line}{lineSuffix} '
 
-        for line_key in largeImageLines:
-            line = f'{largeImageLines[line_key]} '
-            if len(largeImageLines) == 1: rpcLargeImageText = line
+        for line_key in large_image_lines:
+            line = f'{large_image_lines[line_key]} '
+            if len(large_image_lines) == 1: rpc_large_image_text = line
             else:
                 """lineSuffix = "" if len(line) > 20 else (lineLimit - len(line) - sum(_.isupper() for _ in line))*xchar
-                rpcLargeImageText += f'{line}{lineSuffix} '"""
-                rpcLargeImageText += f'{line}{(lineLimit - len(line) - sum(_.isupper() for _ in line))*xchar} '
+                rpc_large_image_text += f'{line}{lineSuffix} '"""
+                rpc_large_image_text += f'{line}{(lineLimit - len(line) - sum(_.isupper() for _ in line))*xchar} '
 
          # if the text is too long, cut it
-        if len(rpcSmallImageText) > 128: rpcSmallImageText = rpcSmallImageText.replace(xchar,'')
-        if len(rpcLargeImageText) > 128: rpcLargeImageText = rpcLargeImageText.replace(xchar,'')
+        if len(rpc_small_image_text) > 128:
+            rpc_small_image_text = rpc_small_image_text.replace(xchar,'')
+        if len(rpc_large_image_text) > 128:
+            rpc_large_image_text = rpc_large_image_text.replace(xchar,'')
 
-        updateAssets = {
+        update_assets = {
             'details': title,
             'buttons': rpcButtons,
-            'small_image': rpcSmallImage,
-            'small_text': rpcSmallImageText,
-            'large_text': rpcLargeImageText,
+            'small_image': rpc_small_image,
+            'small_text': rpc_small_image_text,
+            'large_text': rpc_large_image_text,
             # situation-dependent assets
-            'large_image': 'artwork' if not timeRemainingBool and not albumBool else artwork,
-            'state': trackArtistAlbum if timeRemainingBool and not albumBool else artist,
-            'end': float(time_remaining)+start_time if timeRemainingBool else None}
+            'large_image': 'artwork' if not time_remaining_bool and not album_bool else artwork,
+            'state': track_artist_album if time_remaining_bool and not album_bool else artist,
+            'end': time_remaining + start_time if time_remaining_bool else None}
 
-        if timeRemainingBool:
-            if albumBool: print('Updating status with album, time remaining.')
+        if time_remaining_bool:
+            if album_bool: print('Updating status with album, time remaining.')
             else: print('Updating status without album, time remaining.')
         else:
-            if albumBool: print('Updating status with album, no time remaining')
+            if album_bool: print('Updating status with album, no time remaining')
             else: print('Updating status without album, no time remaining')
         try:
-            RPC.update(**updateAssets)
+            RPC.update(**update_assets)
         except Exception as e:
             print('x', e)
 
