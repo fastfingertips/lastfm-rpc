5a0424917b505e484df4fa7baa0ebf42224a4388
FastFingertips <bugra.coskun@outlook.com>
2024-07-25 01:01:36 +0300

    Update project.py

--- constants/project.py
+++ constants/project.py
@@ -3,6 +3,7 @@ from helpers.system.reader import load_config, load_translations
 USERNAME, API_KEY, API_SECRET, APP_LANG = load_config()
 
 CLIENT_ID = '702984897496875072'
+APP_NAME = "Last.fm Discord Rich Presence"
 TRANSLATIONS = load_translations(APP_LANG)
 DEFAULT_AVATAR_ID = "818148bf682d429dc215c1705eb27b98"
 DEFAULT_AVATAR_URL = f"https://lastfm.freetls.fastly.net/i/u/avatar170s/{DEFAULT_AVATAR_ID}.png"
