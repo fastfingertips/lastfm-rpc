84c94190b92427adb29214d2bed8478bb4e91bd0
FastFingertips <bugra.coskun@outlook.com>
2024-07-25 01:36:29 +0300

    Update project.py

--- constants/project.py
+++ constants/project.py
@@ -1,4 +1,4 @@
-from libs.helpers.system.reader import load_config, load_translations
+from helpers.reader import load_config, load_translations
 
 USERNAME, API_KEY, API_SECRET, APP_LANG = load_config()
 
